0b55c45198d6827377f9158b12017e8b6f2a81af
MiguelSR <miguelsr87@gmail.com>
2015-10-01 20:50:59 +0200

    Added complexity optional field

--- metal_scraper/items.py
+++ metal_scraper/items.py
@@ -9,6 +9,12 @@ import scrapy
 
 class Band(scrapy.Item):
     name = scrapy.Field()
-    link = scrapy.Field()
+    metalarchives_id = scrapy.Field()
     style = scrapy.Field()
     country = scrapy.Field()
+    region = scrapy.Field()
+    city = scrapy.Field()
+    status = scrapy.Field()
+    formation_year = scrapy.Field()
+    breakup_year = scrapy.Field()
+    lyrical_themes = scrapy.Field()
--- metal_scraper/spiders/steelspider.py
+++ metal_scraper/spiders/steelspider.py
@@ -15,6 +15,11 @@ class SteelSpider(scrapy.Spider):
     )
     fetched = 0
 
+    def __init__(self, complexity=0):
+        self.complexity = int(complexity)
+        if self.complexity > 0:
+            self.start_urls = (self.start_urls[0] + "yearCreationFrom=0&yearCreationTo=9999&themes=*&location=*",)
+
     def parse(self, response):
         response_data = json.loads(response.body)
         total_records = response_data['iTotalRecords']
@@ -22,15 +27,27 @@ class SteelSpider(scrapy.Spider):
         for item in response_data['aaData']:
             band = Band()
 
-            match = re.search('<a href="(.*?)">(.*)<\/a>.*', item[0])
+            match = re.search('<a href=".*/(\d+)">(.*)<\/a>.*', item[0])
             band['name'] = match.group(2)
-            band['link'] = match.group(1)
-            band['style'] = item[1]
-            band['country'] = item [2]
+            band['metalarchives_id'] = match.group(1)
+
+            band['style'] = item[1].strip()
+
+            band['country'] = item[2].strip()
+
+            if self.complexity > 0:
+                geo_info = item[3].split(',')
+                band['city'] = geo_info[0].strip()
+                if len(geo_info) > 1:
+                    band['region'] = geo_info[1].strip()
+
+                band['lyrical_themes'] = item[4].strip()
+                band['formation_year'] =  "01/01/" + item[5].strip()
+
             self.fetched += 1
             yield band
 
         if self.fetched < total_records:
-            url = self.start_urls[0] + 'iDisplayStart=%s' % self.fetched
+            url = self.start_urls[0] + '&iDisplayStart=%s' % self.fetched
             yield scrapy.Request(url, callback=self.parse)
         yield
