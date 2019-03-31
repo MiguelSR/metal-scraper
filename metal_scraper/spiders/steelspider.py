c32d99b9dd4bc3312bb8bf3d014126d40b5bac4d
ali neer <ali@alineer.com>
2019-03-31 12:38:00 -0400

    add test_data dir, doc string for steely boi

--- metal_scraper/spiders/steelspider.py
+++ metal_scraper/spiders/steelspider.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
 import json
 import re
-
+import os
 import scrapy
 
 from metal_scraper.items import Band
 
+# TODO work out a better way for setting env vars...docker maybe???
+LOCALHOST = True
 
 class SteelSpider(scrapy.Spider):
+    """
+    SteelSpider is the scraper designed to pull the list of bands and all relevant information about them into a 
+    local .json file. Another scraper will be launched on the band URLs and metal archive ids grabbing all relevant 
+    information.
+    """
     name = "steelspider"
-    allowed_domains = ["metal-archives.com", "metal-archives.local"]
-    start_urls = (
-        # 'http://www.metal-archives.com/search/ajax-advanced/searching/bands/?',
-        # Test URL for debugging
-        'http://metal-archives.local/search.json',
-    )
+    if LOCALHOST:
+        allowed_domains = ["../test_data/search.json"]
+        start_urls = 'http://www.metal-archives.com/search/ajax-advanced/searching/bands/?'
+    else:
+        allowed_domains = ["metal-archives.com", "metal-archives.local"]
+        start_urls = (
+            'http://www.metal-archives.com/search/ajax-advanced/searching/bands/?'
+        )
     fetched = 0
 
     def __init__(self, complexity=0):
