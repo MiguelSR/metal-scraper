cd859a2cd62756e924c8b69de874b3f25fda430b
ali neer <ali@alineer.com>
2019-03-31 23:23:34 -0400

    disco scraper 🕺🕺🕺🕺🕺

--- metal_scraper/spiders/ironspider.py
+++ metal_scraper/spiders/ironspider.py
@@ -11,9 +11,10 @@ from metal_scraper.items import Band
 log = logging.getLogger('ironspider')
 log.setLevel(logging.DEBUG)
 
+LOCALHOST = True
 
 def run(path):
-    bands = get_records(path)
+    bands = get_bandlist(path)
 
     for band in bands:
         url = band['url']
@@ -21,36 +22,78 @@ def run(path):
         page = urllib.request.urlopen(url)
 
         soup = BeautifulSoup(page, 'html.parser')
-
         # Get the band logo
         logo_div = soup.find("a", {"id": "logo"})
         band['logo'] = logo_div['href']
-
-        # Band stats:
-        dts = soup.find_all("dt")
-        stats_keys = []
-        for key in dts:
-            stats_keys.append(key.get_text().lower().replace(" ", "_"))
-        print(f"stats_keys: {stats_keys}")
-        dds = soup.find_all("dd")
-        stats_values = []
-        for value in dds:
-            stats_values.append(value.get_text().lower())
-        band_stats = dict(zip(stats_keys, stats_values))
-        # m-a sucks, let's do some formatting here before adding the stat block to the stupid fucking 
-        # shitty dictionary
-        band.update(band_stats)
-
+        band.update(get_band_stats(soup))
+        band["albums"] = get_complete_discography(band["metalarchives_id"])
         print(f'band: {band}')
-
-
-
 
 # gets a list of records to start crawling urls
 # should probably be refactored into a db process
-def get_records(path):
+def get_bandlist(path):
+    """
+    returns list of bands urls to be crawled
+    """
     if path:
         with open(path, 'r') as f:
             bands = json.load(f)
         return bands
     # raise error
+
+def get_band_stats(soup):
+    """
+    Returns all statistical information about a band.
+    """
+    dts = soup.find_all("dt")
+    stats_keys = []
+    for key in dts:
+        stats_keys.append(key.get_text().lower().replace(" ", "_").strip(":"))
+    dds = soup.find_all("dd")
+    stats_values = []
+    for value in dds:
+        stats_values.append(value.get_text().lower().replace("\n", " ").replace("\t", " ").strip())
+    band_stats = dict(zip(stats_keys, stats_values))
+    return band_stats
+
+def get_complete_discography(band_id):
+    """
+    returns the discography for a band
+    fun fact: we actually just: 'https://www.metal-archives.com/band/discography/id/3540438154/tab/all' is a link to the full disco
+    construction: ma/band/discography/id/<ma_id>/tab/all
+    """
+    # construct URL
+    url = f"https://wwww.metal-archives.com/band/discography/id/{band_id}/tab/all"
+    if LOCALHOST:
+        url = "http://localhost:8000/metal_scraper/test_data/disco.html"
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+
+    rows = soup.find_all("tr")
+    albums = []
+    
+    for row in rows:
+        cols = row.findAll('td')
+        album = {}
+        for idx, col in enumerate(cols):
+            # python is really stupid for not having a switch...
+            # set up name and url 
+            if idx == 0:
+                album["name"] = col.find("a").get_text()
+                album["url"] = col.find("a")["href"]
+                album["album_id"] = album["url"].split("/")[-1]
+            elif idx == 1:
+                album["type"] = col.get_text().strip()
+            elif idx == 2:
+                album["year"] = col.get_text().strip()
+            elif idx == 3:
+                if(col.find("a")):
+                    review = {}
+                    review["percent_and_count"] = col.find("a").get_text()
+                    review["url"] = col.find("a")["href"]
+                    album["review"] = review
+            else: # set to null
+                album = None
+        if album:
+            albums.append(album)
+    return(albums)
